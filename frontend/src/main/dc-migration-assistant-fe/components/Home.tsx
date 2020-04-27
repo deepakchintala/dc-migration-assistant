0c4087b8d761253bdbb1363110317931e3100174
Ben Partridge <bpartridge@atlassian.com>
2020-04-27 16:42:10 +1000

    CHET-285: implement rendering a message when the instance is already in a migration

--- frontend/src/main/dc-migration-assistant-fe/components/Home.tsx
+++ frontend/src/main/dc-migration-assistant-fe/components/Home.tsx
@@ -14,12 +14,14 @@
  * limitations under the License.
  */
 
-import React, { ReactElement, useState } from 'react';
+import React, { ReactElement, useState, useEffect, FunctionComponent } from 'react';
 import Button from '@atlaskit/button';
 import InlineMessage from '@atlaskit/inline-message';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import { I18n } from '@atlassian/wrm-react-i18n';
+import SectionMessage from '@atlaskit/section-message';
+import Spinner from '@atlaskit/spinner';
 
 import { startPath } from '../utils/RoutePaths';
 import { migration } from '../api/migration';
@@ -47,7 +49,15 @@ const InfoProps = {
     secondaryText: I18n.getText('atlassian.migration.datacenter.home.info.content'),
 };
 
-export const Home = ({ title, synopsis, startButtonText }: HomeProps): ReactElement => {
+type ActionSectionProps = {
+    canStart: boolean;
+    startButtonText: string;
+};
+
+const MigrationActionSection: FunctionComponent<ActionSectionProps> = ({
+    startButtonText,
+    canStart,
+}) => {
     const [error, setError] = useState<string>();
     const [loading, setLoading] = useState<boolean>(false);
 
@@ -66,24 +76,58 @@ export const Home = ({ title, synopsis, startButtonText }: HomeProps): ReactElem
             });
     };
 
+    if (canStart) {
+        return (
+            <>
+                <ErrorFlag
+                    showError={error && error !== ''}
+                    dismissErrorFunc={(): void => setError('')}
+                    // FIXME: Internationalisation
+                    title="Unable to create migration"
+                    description={error}
+                    id="migration-creation-error"
+                />
+                <InlineMessage {...InfoProps} />
+                <ButtonContainer>
+                    <Button isLoading={loading} appearance="primary" onClick={createMigration}>
+                        {startButtonText}
+                    </Button>
+                </ButtonContainer>
+            </>
+        );
+    }
+    return <SectionMessage appearance="warning">You are already in a migration.</SectionMessage>;
+};
+
+export const Home = ({ title, synopsis, startButtonText }: HomeProps): ReactElement => {
+    const [canStart, setCanStart] = useState<boolean>(false);
+    const [loadingCanStart, setLoadingCanStart] = useState<boolean>(true);
+
+    useEffect(() => {
+        setLoadingCanStart(true);
+        migration
+            .getMigrationStage()
+            .then(stage => {
+                if (stage === 'not_started') {
+                    setCanStart(true);
+                } else {
+                    setCanStart(false);
+                }
+            })
+            .finally(() => {
+                setLoadingCanStart(false);
+            });
+    }, []);
+
     return (
         <HomeContainer>
             <h2>{title}</h2>
             <p>{synopsis}</p>
-            <ErrorFlag
-                showError={error && error !== ''}
-                dismissErrorFunc={(): void => setError('')}
-                // FIXME: Internationalisation
-                title="Unable to create migration"
-                description={error}
-                id="migration-creation-error"
-            />
-            <InlineMessage {...InfoProps} />
-            <ButtonContainer>
-                <Button isLoading={loading} appearance="primary" onClick={createMigration}>
-                    {startButtonText}
-                </Button>
-            </ButtonContainer>
+            {loadingCanStart ? (
+                <Spinner />
+            ) : (
+                <MigrationActionSection startButtonText={startButtonText} canStart={canStart} />
+            )}
         </HomeContainer>
     );
 };
