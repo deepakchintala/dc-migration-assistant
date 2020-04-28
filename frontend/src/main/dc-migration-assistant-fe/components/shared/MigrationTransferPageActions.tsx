ac0fd3053ae87cfb712ace2ba67994ec4d2bd222
Ben Partridge <bpartridge@atlassian.com>
2020-04-28 11:43:56 +1000

    CHET-289: Add ability to specify where the next button of a transfer page takes the user

--- frontend/src/main/dc-migration-assistant-fe/components/shared/MigrationTransferPageActions.tsx
+++ frontend/src/main/dc-migration-assistant-fe/components/shared/MigrationTransferPageActions.tsx
@@ -4,20 +4,51 @@ import { Link } from 'react-router-dom';
 import { I18n } from '@atlassian/wrm-react-i18n';
 import { overviewPath } from '../../utils/RoutePaths';
 
+/**
+ * A component which renders the actions that can be taken at a given step of the migration.
+ * The "Cancel" action is always available and cancels the current step. There are three other
+ * possible actions which can be availabl depending on the props:
+ *   **Start**: Will be available when the current transfer hasn't started
+ *   **Refresh**: Will be available when the current transfer is in progress
+ *   **Next**: Will be available when the current transfer is completed. It's text can be overriden
+ */
 type MigrationTransferActionsProps = {
-    updateTransferProgress: () => Promise<void>;
+    /**
+     * A function which will be called when the Refresh button is clicked
+     */
+    onRefresh: () => Promise<void>;
+    /**
+     * Whether the current migration transfer has completed or not
+     */
     finished: boolean;
+    /**
+     * The text to display on the button that takes the user to the next step of the migration
+     */
     nextText: string;
+    /**
+     * The path to take the user to whent hey click the "next" action button
+     */
+    nextRoute: string;
+    /**
+     * A function which will be called when the "start" action button is called.
+     */
     startMigrationPhase: () => Promise<void>;
+    /**
+     * Whether the current migration transfer has already started or not
+     */
     started: boolean;
+    /**
+     * Should be true if any of the above properties are being fetched
+     */
     loading: boolean;
 };
 
 export const MigrationTransferActions: FunctionComponent<MigrationTransferActionsProps> = ({
     finished,
     nextText,
+    nextRoute,
     startMigrationPhase,
-    updateTransferProgress,
+    onRefresh: updateTransferProgress,
     started,
     loading,
 }) => {
@@ -52,9 +83,11 @@ export const MigrationTransferActions: FunctionComponent<MigrationTransferAction
 
     if (finished) {
         const NextButton = (
-            <Button style={defaultButtonStyle} appearance="primary">
-                {nextText}
-            </Button>
+            <Link to={nextRoute}>
+                <Button style={defaultButtonStyle} appearance="primary">
+                    {nextText}
+                </Button>
+            </Link>
         );
         ActionButton = NextButton;
     }
