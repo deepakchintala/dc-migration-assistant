db6417ae63933642852f69b48035e339a74cb53c
Adam Brokes <abrokes@atlassian.com>
2020-05-01 12:36:26 +1000

    CHET-315: Change numbering for the steps

--- frontend/src/main/dc-migration-assistant-fe/components/warning/WarningStage.tsx
+++ frontend/src/main/dc-migration-assistant-fe/components/warning/WarningStage.tsx
@@ -4,10 +4,14 @@ import { Checkbox } from '@atlaskit/checkbox';
 import Button from '@atlaskit/button';
 import { Redirect } from 'react-router-dom';
 import { dbPath } from '../../utils/RoutePaths';
+import { I18n } from '../../atlassian/mocks/@atlassian/wrm-react-i18n';
 
 export const WarningStagePage: FunctionComponent = () => {
     const [agreed, setAgreed] = useState<boolean>(false);
     const [shouldRedirect, setShouldRedirect] = useState<boolean>(false);
+
+    const heading = I18n.getText('atlassian.migration.datacenter.db.title');
+    const description = I18n.getText('atlassian.migration.datacenter.db.description');
 
     const handleClick = (): void => {
         if (agreed) {
@@ -25,12 +29,8 @@ export const WarningStagePage: FunctionComponent = () => {
 
     return (
         <div>
-            <h1>Step 4 of 7: Redirect users</h1>
-            <p>
-                Take your Jira instance offline to prevent users from accessing it. This will allow
-                us to sync your database properly. User access during this phase could prevent some
-                data from being copied.
-            </p>
+            <h1>{heading}</h1>
+            <p>{description}</p>
             <SectionMessage appearance="info" title="To take your Jira instance offline:">
                 <ol>
                     <li>Make sure that users are logged out</li>
