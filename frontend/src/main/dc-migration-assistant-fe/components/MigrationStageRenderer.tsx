c449df536a33ed8eda5d7c219bb33a49ba6d3831
Ben Partridge <bpartridge@atlassian.com>
2020-05-04 11:28:46 +1000

    CHET-336: implement component that redirects to current stage of migration

---
+++ frontend/src/main/dc-migration-assistant-fe/components/MigrationStageRenderer.tsx
@@ -0,0 +1,24 @@
+import React, { FunctionComponent, useState, useEffect } from 'react';
+import { Redirect } from 'react-router-dom';
+import Spinner from '@atlaskit/spinner';
+import { getPathForStage } from '../utils/migration-stage-to-path';
+import { migration, MigrationStage } from '../api/migration';
+
+export const MigrationStageRenderer: FunctionComponent = () => {
+    const [loading, setLoading] = useState<boolean>(true);
+    const [currentStage, setStage] = useState<MigrationStage>();
+
+    useEffect(() => {
+        setLoading(true);
+        migration.getMigrationStage().then(stage => {
+            setStage(stage);
+            setLoading(false);
+        });
+    }, []);
+
+    if (loading) {
+        return <Spinner />;
+    }
+
+    return <Redirect to={getPathForStage(currentStage)} />;
+};
