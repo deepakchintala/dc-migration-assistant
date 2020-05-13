53102eff412aafbb6b5545e603d54dffd3e1c773
Steve Smith <ssmith@atlassian.com>
2020-05-14 08:49:55 +1000

    CHET-388: Add screenshotting.

--- jira-e2e-tests/integration/mock_migration.ts
+++ jira-e2e-tests/integration/mock_migration.ts
@@ -15,6 +15,14 @@ describe('Database Migration page', () => {
     });
 
     it('End to end mock PoC', () => {
+
+        // Initial/global mock responses
+        cy.route({
+            method: 'GET',
+            url: '/jira/rest/dc-migration/1.0/migration',
+            response: {"stage":"not_started"}
+        }).as('stage_not_started');
+
         cy.route({
             method: 'GET',
             url: '/jira/rest/dc-migration/1.0/migration/ready',
@@ -23,23 +31,11 @@ describe('Database Migration page', () => {
                 osCompatible: true,
                 fsSizeCompatible: true,
             },
-        });
+        }).as("ready");
         cy.route({
             method: 'POST',
             url: '/jira/rest/dc-migration/1.0/migration',
             response: {},
-        });
-
-        // Home; should be no migration; start one
-        cy.visit(jira.migrationHome);
-        cy.get('[data-test=start-migration]')
-            .should('exist')
-            .click();
-
-
-        // AWS auth page.
-        cy.location().should((loc: Location) => {
-            expect(loc.pathname).to.eq("/jira/plugins/servlet/dc-migration-assistant/aws/auth")
         });
 
         cy.route({
@@ -49,17 +45,6 @@ describe('Database Migration page', () => {
             response: {},
         });
 
-        cy.get('[data-test=aws-auth-key]').type('AWS_KEY');
-        cy.get('[data-test=aws-auth-secret]').type('AWS_SECRET');
-        // FIXME: This may be flaky; the AtlasKit AsyncSelect
-        // component is hard to instrument.
-        cy.get('#region-uid3').click();
-        cy.get(`[id^=react-select]:contains(ap-southeast-2)`).click();
-        cy.get('[data-test=aws-auth-submit]').click();
-
-
-        // Quickstart page; bare minimum config
-        // Note: These names are generated from the QS yaml
         cy.route({
             method: 'GET',
             url: '/jira/rest/dc-migration/1.0/aws/availabilityZones',
@@ -72,6 +57,47 @@ describe('Database Migration page', () => {
             response: {},
         });
 
+        // ******************** start ******************** //
+
+        // Home; should be no migration; start one
+        cy.visit(jira.migrationHome);
+        cy.wait('@ready');
+        cy.screenshot("home-ready-ok")
+
+        cy.get('[data-test=start-migration]')
+            .should('exist')
+            .click();
+
+        cy.route({
+            method: 'GET',
+            url: '/jira/rest/dc-migration/1.0/migration',
+            response: {"stage":"provision_application_wait"}
+        }).as('stage_provision_application_wait');
+
+        // AWS auth page.
+        cy.location().should((loc: Location) => {
+            expect(loc.pathname).to.eq("/jira/plugins/servlet/dc-migration-assistant/aws/auth")
+        });
+
+        cy.get('[data-test=aws-auth-key]').type('AWS_KEY');
+        cy.get('[data-test=aws-auth-secret]').type('AWS_SECRET');
+        // FIXME: This may be flaky; the AtlasKit AsyncSelect
+        // component is hard to instrument.
+        cy.get('#region-uid3').click();
+        cy.get(`[id^=react-select]:contains(ap-southeast-2)`).click();
+
+        cy.screenshot("aws-auth")
+        cy.get('[data-test=aws-auth-submit]').click();
+
+
+        // Quickstart page; bare minimum config
+        // Note: These names are generated from the QS yaml
+        cy.route({
+            method: 'GET',
+            url: '/jira/rest/dc-migration/1.0/aws/stack/status',
+            response: {"status":{"state":"CREATE_IN_PROGRESS","reason":"User Initiated"},"phase":"app_infra"}
+        }).as('status1');
+
         cy.get('[name=stackName]').type('teststack');
         cy.get('[name=DBMasterUserPassword]').type('LKJLKJLlkjlkjl7987987#');
         cy.get('[name=DBPassword]').type('LKJLKJLlkjlkjl7987987#');
@@ -83,27 +109,17 @@ describe('Database Migration page', () => {
         cy.get('#AvailabilityZones-uid28').click();
         cy.get('#react-select-11-option-1').click();
         cy.get('[name=ExportPrefix]').type('TEST-VPC-');
+
+        cy.screenshot("quickstart-params")
         cy.get('[data-test=qs-submit]').click();
 
-        cy.route({
-            method: 'GET',
-            url: '/jira/rest/dc-migration/1.0/migration',
-            response: {"stage":"provision_application_wait"}
-        });
-
-        cy.route({
-            method: 'GET',
-            url: '/jira/rest/dc-migration/1.0/aws/stack/status',
-            response: {"status":{"state":"CREATE_IN_PROGRESS","reason":"User Initiated"},"phase":"app_infra"}
-        }).as('status1');
-        cy.wait('@status1');
-
-        cy.route({
-            method: 'GET',
-            url: '/jira/rest/dc-migration/1.0/aws/stack/status',
-            response: {"status":{"state":"CREATE_COMPLETE","reason":"User Initiated"},"phase":"app_infra"}
-        }).as('status2');
-        cy.wait('@status2');
+//        cy.wait('@status1', {timeout: 60000});
+        // cy.route({
+        //     method: 'GET',
+        //     url: '/jira/rest/dc-migration/1.0/aws/stack/status',
+        //     response: {"status":{"state":"CREATE_COMPLETE","reason":"User Initiated"},"phase":"app_infra"}
+        // }).as('status2');
+        // cy.wait('@status2', {timeout: 60000});
 
     });
 
