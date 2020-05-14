88b861894cde5b0fb1c9c7433aa738e14f699499
Steve Smith <ssmith@atlassian.com>
2020-05-14 16:00:32 +1000

    CHET-388: Fix logins with context.

--- jira-e2e-tests/integration/amps_mocked.ts
+++ jira-e2e-tests/integration/amps_mocked.ts
@@ -3,20 +3,24 @@ import * as jira from '../support';
 import * as scenarios from '../support/scenarios';
 
 describe('Database Migration page', () => {
+    const ctx = jira.amps_context;
+
     beforeEach(() => {
         cy.on('uncaught:exception', (err, runnable) => false);
 
+        // Delete `fetch()` to force use of XHR for
+        // Cypress. Atlaskit/Jira will polyfill this.
         Cypress.on('window:before:load', (win) => {
             delete win.fetch
         })
         cy.server();
 
-        cy.jira_login('admin', 'admin');
+        cy.jira_login(ctx, 'admin', 'admin');
     });
 
     it('End to end mocked', () => {
 
-        scenarios.mock_end2end(jira.amps_context);
+        scenarios.mock_end2end(ctx);
 
     });
 
