4c60b7365f9969417cdb07942def9daeb53a9229
Ben Partridge <bpartridge@atlassian.com>
2020-05-26 14:10:28 +1000

    CHET-266: [filesystem-processor] use trailing '/' to detect if S3 object is directory or file

--- filesystem-processor/src/main/kotlin/com/atlassian/migration/datacenter/fs/processor/services/S3ToFileWriter.kt
+++ filesystem-processor/src/main/kotlin/com/atlassian/migration/datacenter/fs/processor/services/S3ToFileWriter.kt
@@ -2,7 +2,6 @@ package com.atlassian.migration.datacenter.fs.processor.services
 
 import com.amazonaws.services.s3.AmazonS3
 import com.amazonaws.services.s3.event.S3EventNotification
-import lombok.SneakyThrows
 import org.apache.commons.io.IOUtils
 import org.slf4j.LoggerFactory
 import java.io.File
@@ -10,23 +9,23 @@ import java.io.FileOutputStream
 import java.io.IOException
 import java.net.URLDecoder
 import java.nio.charset.Charset
-import java.nio.file.Paths
 
-class S3ToFileWriter(private val s3Client: AmazonS3?, private val entity: S3EventNotification.S3Entity?, private val jiraHome: String?) : Runnable {
+class S3ToFileWriter(private val s3Client: AmazonS3, private val entity: S3EventNotification.S3Entity, private val jiraHome: String) : Runnable {
 
     private val log = LoggerFactory.getLogger(S3ToFileWriter::class.java)
 
-    @SneakyThrows
     override fun run() {
-        val key = URLDecoder.decode(entity!!.getObject().key, Charset.defaultCharset().toString())
+        val key = URLDecoder.decode(entity.getObject().key, Charset.defaultCharset().toString())
         try {
-            s3Client!!.getObject(entity.bucket.name, key).use { s3object ->
-                s3object.objectContent.use { inputStream ->
-                    val localPath = File("$jiraHome/$key")
-                    val keyFile: String = Paths.get(localPath.path).fileName.toString()
-                    log.info("Got request to write file: $keyFile")
-                    // FIXME: Is this determining if the object is a file by checking if it has a file extension?
-                    if (keyFile.contains(".")) {
+            s3Client.getObject(entity.bucket.name, key).use { s3object ->
+                val absolutePathString = "$jiraHome/$key"
+                val localPath = File(absolutePathString)
+                if (key.endsWith("/")) {
+                    log.info("Got request to create directory: $absolutePathString")
+                    localPath.mkdirs()
+                } else {
+                    s3object.objectContent.use { inputStream ->
+                        log.info("Got request to write file: $absolutePathString")
                         if (!localPath.parentFile.exists()) {
                             if (localPath.parentFile.mkdirs()) {
                                 log.info("Made the missing parent directory {}", localPath.path)
@@ -34,18 +33,16 @@ class S3ToFileWriter(private val s3Client: AmazonS3?, private val entity: S3Even
                         }
                         try {
                             IOUtils.copy(inputStream, FileOutputStream(localPath))
+                            log.info("Successfully wrote: $absolutePathString")
                         } catch (e: IOException) {
-                            log.error("Failed to write file $keyFile", e)
+                            log.error("Failed to write file $absolutePathString", e)
                         }
-                    } else {
-                        localPath.mkdirs()
                     }
                 }
             }
         } catch (ex: Exception) {
             log.error("Failed to process " + ex.localizedMessage)
-            log.error(ex.cause!!.localizedMessage)
+            log.error(ex.cause?.localizedMessage)
         }
     }
-
 }
