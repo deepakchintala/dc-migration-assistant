a83fee78dd56a6ab5436507bdd64311642505586
Andy Powell <andpow@amazon.de>
2020-05-26 07:28:50 +0200

    merge

--- ec2-mock-metadata/src/test/kotlin/com/atlassian/migration/datacenter/TestConfiguration.kt
+++
@@ -1,8 +0,0 @@
-package com.atlassian.migration.datacenter
-
-import org.springframework.context.annotation.ComponentScan
-import org.springframework.context.annotation.Configuration
-
-@Configuration
-open class TestConfiguration {
-}
