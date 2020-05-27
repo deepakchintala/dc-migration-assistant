1fc06079d0688d2631fddaaf955e3ff84a7e4543
Ben Partridge <bpartridge@atlassian.com>
2020-05-27 11:31:07 +1000

    CHET-423: implement conditional rendering of inpt for ASI. Don't use atlaskit form because it doesn't like it when the form changes

--- frontend/src/main/dc-migration-assistant-fe/components/aws/quickstart/asi/ExistingASIConfiguration.tsx
+++ frontend/src/main/dc-migration-assistant-fe/components/aws/quickstart/asi/ExistingASIConfiguration.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, ReactNode } from 'react';
+import React, { FunctionComponent, ReactNode, useState } from 'react';
 import SectionMessage from '@atlaskit/section-message';
 import Form, { Field, HelperMessage } from '@atlaskit/form';
 import { RadioGroup } from '@atlaskit/radio';
@@ -6,6 +6,7 @@ import TextField from '@atlaskit/textfield';
 import styled from 'styled-components';
 import { ButtonGroup } from '@atlaskit/button';
 import { Button } from '@atlaskit/button/dist/cjs/components/Button';
+import { AsyncSelect, OptionType } from '@atlaskit/select';
 import { CancelButton } from '../../../shared/CancelButton';
 
 const radioValues = [
@@ -14,8 +15,8 @@ const radioValues = [
 ];
 
 type ExistingASIConfigurationFormData = {
-    deploymentMode: 'existing' | 'new';
-    ASIIdentifier: string;
+    ASIIdentifierExisting: string;
+    ASIIdentifierNew: string;
 };
 
 const RequiredStar = styled.span`
@@ -26,7 +27,15 @@ const ButtonRow = styled.div`
     margin: 15px 0px 0px 10px;
 `;
 
+const asyncASIPrefixOptions = (): Promise<Array<OptionType>> =>
+    Promise.resolve([
+        { label: 'ATL-', value: 'ATL-', key: 'ATL-' },
+        { label: 'BP-', value: 'BP-', key: 'BP-' },
+    ]);
+
 export const ExistingASIConfiguration: FunctionComponent = () => {
+    const [useExisting, setUseExisting] = useState<boolean>(true);
+
     return (
         <div>
             <h1>Step 2 of 7: Configure ASI</h1>
@@ -44,38 +53,36 @@ export const ExistingASIConfiguration: FunctionComponent = () => {
             <h5>
                 Deploy to<RequiredStar>*</RequiredStar>
             </h5>
-            <RadioGroup options={radioValues} defaultValue={radioValues[0].value} />
-            <Form<ExistingASIConfigurationFormData>
-                onSubmit={(data): void => console.log('frm data', data)}
-            >
-                {({ formProps }): ReactNode => (
-                    <form {...formProps}>
-                        <Field name="ASIIdentifier" label="ASI identifier" isRequired>
-                            {({ fieldProps }): ReactNode => (
-                                <>
-                                    <TextField placeholder="ATL-" width="xlarge" {...fieldProps} />
-                                    <HelperMessage>
-                                        Identifier used in all variables (VPCID, SubnetIDs, KeyName)
-                                        exported from this deployment&apos;s Atlassian Standard
-                                        Infrastructure. Use different identifiers if you&apos;re
-                                        deploying multiple Atlassian Standard Infrastructures in the
-                                        same AWS region. Format is 3 upper case letters followed by
-                                        ”-“.
-                                    </HelperMessage>
-                                </>
-                            )}
-                        </Field>
-                        <ButtonRow>
-                            <ButtonGroup>
-                                <Button type="submit" appearance="primary" data-test="asi-submit">
-                                    Next
-                                </Button>
-                                <CancelButton />
-                            </ButtonGroup>
-                        </ButtonRow>
-                    </form>
-                )}
-            </Form>
+            <RadioGroup
+                options={radioValues}
+                defaultValue={radioValues[0].value}
+                onChange={(event): void => setUseExisting(event.currentTarget.value === 'existing')}
+            />
+            {useExisting ? (
+                <AsyncSelect
+                    className="asi-select"
+                    cacheOptions
+                    defaultOptions
+                    loadOptions={asyncASIPrefixOptions}
+                    data-test="asi-select"
+                />
+            ) : (
+                <TextField placeholder="ATL-" width="xlarge" />
+            )}
+            <HelperMessage>
+                Identifier used in all variables (VPCID, SubnetIDs, KeyName) exported from this
+                deployment&apos;s Atlassian Standard Infrastructure. Use different identifiers if
+                you&apos;re deploying multiple Atlassian Standard Infrastructures in the same AWS
+                region. Format is 3 upper case letters followed by ”-“.
+            </HelperMessage>
+            <ButtonRow>
+                <ButtonGroup>
+                    <Button type="submit" appearance="primary" data-test="asi-submit">
+                        Next
+                    </Button>
+                    <CancelButton />
+                </ButtonGroup>
+            </ButtonRow>
         </div>
     );
 };
