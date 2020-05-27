1cb9c9aa484a4770ba98ff818a2075c7255f6156
Ben Partridge <bpartridge@atlassian.com>
2020-05-27 10:59:28 +1000

    CHET-423: add buttons for ASI configuration

--- frontend/src/main/dc-migration-assistant-fe/components/aws/quickstart/asi/ExistingASIConfiguration.tsx
+++ frontend/src/main/dc-migration-assistant-fe/components/aws/quickstart/asi/ExistingASIConfiguration.tsx
@@ -3,6 +3,10 @@ import SectionMessage from '@atlaskit/section-message';
 import Form, { Field, HelperMessage } from '@atlaskit/form';
 import { RadioGroup } from '@atlaskit/radio';
 import TextField from '@atlaskit/textfield';
+import styled from 'styled-components';
+import { ButtonGroup } from '@atlaskit/button';
+import { Button } from '@atlaskit/button/dist/cjs/components/Button';
+import { CancelButton } from '../../../shared/CancelButton';
 
 const radioValues = [
     { name: 'deploymentMode', value: 'existing', label: 'Existing ASI' },
@@ -13,6 +17,14 @@ type ExistingASIConfigurationFormData = {
     deploymentMode: 'existing' | 'new';
     ASIIdentifier: string;
 };
+
+const RequiredStar = styled.span`
+    color: #de350b;
+`;
+
+const ButtonRow = styled.div`
+    margin: 15px 0px 0px 10px;
+`;
 
 export const ExistingASIConfiguration: FunctionComponent = () => {
     return (
@@ -30,7 +42,7 @@ export const ExistingASIConfiguration: FunctionComponent = () => {
                 </p>
             </SectionMessage>
             <h5>
-                Deploy to<span>*</span>
+                Deploy to<RequiredStar>*</RequiredStar>
             </h5>
             <RadioGroup options={radioValues} defaultValue={radioValues[0].value} />
             <Form<ExistingASIConfigurationFormData>
@@ -41,11 +53,11 @@ export const ExistingASIConfiguration: FunctionComponent = () => {
                         <Field name="ASIIdentifier" label="ASI identifier" isRequired>
                             {({ fieldProps }): ReactNode => (
                                 <>
-                                    <TextField {...fieldProps} />
+                                    <TextField placeholder="ATL-" width="xlarge" {...fieldProps} />
                                     <HelperMessage>
                                         Identifier used in all variables (VPCID, SubnetIDs, KeyName)
-                                        exported from this deployment&aposs Atlassian Standard
-                                        Infrastructure. Use different identifiers if you&aposre
+                                        exported from this deployment&apos;s Atlassian Standard
+                                        Infrastructure. Use different identifiers if you&apos;re
                                         deploying multiple Atlassian Standard Infrastructures in the
                                         same AWS region. Format is 3 upper case letters followed by
                                         ”-“.
@@ -53,6 +65,14 @@ export const ExistingASIConfiguration: FunctionComponent = () => {
                                 </>
                             )}
                         </Field>
+                        <ButtonRow>
+                            <ButtonGroup>
+                                <Button type="submit" appearance="primary" data-test="asi-submit">
+                                    Next
+                                </Button>
+                                <CancelButton />
+                            </ButtonGroup>
+                        </ButtonRow>
                     </form>
                 )}
             </Form>
