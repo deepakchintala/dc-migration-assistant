fdb52f3a76be5a0347f187f708c0a9d6400da174
Steve Smith <ssmith@atlassian.com>
2020-06-19 11:00:04 +1000

    CHET-199: Add report manager.

---
+++ core/src/main/kotlin/com/atlassian/migration/datacenter/core/fs/DefaultFileSystemMigrationReportManager.kt
@@ -0,0 +1,27 @@
+package com.atlassian.migration.datacenter.core.fs
+
+import com.atlassian.migration.datacenter.core.fs.reporting.DefaultFileSystemMigrationReport
+import com.atlassian.migration.datacenter.spi.fs.reporting.FileSystemMigrationReport
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Component
+
+@Component
+class DefaultFileSystemMigrationReportManager : FileSystemMigrationReportManager {
+    private val logger = LoggerFactory.getLogger(javaClass)
+
+    val reports: HashMap<ReportType, FileSystemMigrationReport> = hashMapOf(
+            ReportType.Filesystem to DefaultFileSystemMigrationReport(),
+            ReportType.Database to DefaultFileSystemMigrationReport(),
+            ReportType.Final to DefaultFileSystemMigrationReport()
+    )
+
+    override fun resetReport(type: ReportType): FileSystemMigrationReport {
+        val report = DefaultFileSystemMigrationReport()
+        reports[type] = report
+        return report
+    }
+
+    override fun getCurrentReport(type: ReportType): FileSystemMigrationReport? {
+        return reports[type]
+    }
+}
