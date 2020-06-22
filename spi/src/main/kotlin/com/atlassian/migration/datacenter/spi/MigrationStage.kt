405bb49f1213274f2a2dc72950f78d658a98f32f
Ben Partridge <bpartridge@atlassian.com>
2020-06-22 16:16:58 +1000

    CHET-528: [spi] refactor migration stage so that retry transitions are handled the same as all other transitions; add retry transition for final sync

--- spi/src/main/kotlin/com/atlassian/migration/datacenter/spi/MigrationStage.kt
+++ spi/src/main/kotlin/com/atlassian/migration/datacenter/spi/MigrationStage.kt
@@ -19,47 +19,78 @@ package com.atlassian.migration.datacenter.spi
  * Represents all possible states of an on-premise to cloud migration.
  */
 enum class MigrationStage {
-    NOT_STARTED(),
-    AUTHENTICATION(NOT_STARTED),
+    NOT_STARTED {
+        override val validAncestorStages = emptySet<MigrationStage>()
+    },
+    AUTHENTICATION {
+        override val validAncestorStages = setOf(NOT_STARTED)
+    },
+    PROVISION_APPLICATION {
+        override val validAncestorStages = setOf(AUTHENTICATION)
+    },
+    PROVISION_APPLICATION_WAIT {
+        override val validAncestorStages = setOf(PROVISION_APPLICATION)
+    },
+    PROVISION_MIGRATION_STACK {
+        override val validAncestorStages = setOf(PROVISION_APPLICATION_WAIT)
+    },
+    PROVISION_MIGRATION_STACK_WAIT {
+        override val validAncestorStages = setOf(PROVISION_MIGRATION_STACK)
+    },
+    PROVISIONING_ERROR {
+        override val validAncestorStages = setOf(PROVISION_APPLICATION, PROVISION_APPLICATION_WAIT, PROVISION_MIGRATION_STACK, PROVISION_MIGRATION_STACK_WAIT)
+    },
+    FS_MIGRATION_COPY {
+        override val validAncestorStages = setOf(PROVISION_MIGRATION_STACK_WAIT)
+    },
+    FS_MIGRATION_COPY_WAIT {
+        override val validAncestorStages = setOf(FS_MIGRATION_COPY)
+    },
+    FS_MIGRATION_ERROR {
+        override val validAncestorStages =  setOf(FS_MIGRATION_COPY, FS_MIGRATION_COPY_WAIT)
+    },
+    OFFLINE_WARNING {
+        override val validAncestorStages = setOf(FS_MIGRATION_COPY_WAIT)
+    },
 
-    PROVISION_APPLICATION(AUTHENTICATION),
-    PROVISION_APPLICATION_WAIT(PROVISION_APPLICATION),
-    PROVISION_MIGRATION_STACK(PROVISION_APPLICATION_WAIT),
-    PROVISION_MIGRATION_STACK_WAIT(PROVISION_MIGRATION_STACK),
-    PROVISIONING_ERROR(PROVISION_APPLICATION, PROVISION_APPLICATION_WAIT, PROVISION_MIGRATION_STACK, PROVISION_MIGRATION_STACK_WAIT),
+    DB_MIGRATION_EXPORT {
+        override val validAncestorStages: Set<MigrationStage>
+            get() = setOf(OFFLINE_WARNING, FINAL_SYNC_ERROR)
+    },
+    DB_MIGRATION_EXPORT_WAIT {
+        override val validAncestorStages = setOf(DB_MIGRATION_EXPORT)
+    },
+    DB_MIGRATION_UPLOAD {
+        override val validAncestorStages = setOf(DB_MIGRATION_EXPORT_WAIT)
+    },
+    DB_MIGRATION_UPLOAD_WAIT {
+        override val validAncestorStages = setOf(DB_MIGRATION_UPLOAD)
+    },
+    DATA_MIGRATION_IMPORT {
+        override val validAncestorStages = setOf(DB_MIGRATION_UPLOAD_WAIT)
+    },
+    DATA_MIGRATION_IMPORT_WAIT {
+        override val validAncestorStages = setOf(DATA_MIGRATION_IMPORT)
+    },
+    FINAL_SYNC_WAIT {
+        override val validAncestorStages = setOf(DATA_MIGRATION_IMPORT_WAIT)
+    },
+    FINAL_SYNC_ERROR {
+        override val validAncestorStages = setOf(DB_MIGRATION_EXPORT, DB_MIGRATION_EXPORT_WAIT, DB_MIGRATION_UPLOAD, DB_MIGRATION_UPLOAD_WAIT, DATA_MIGRATION_IMPORT, DATA_MIGRATION_IMPORT_WAIT, FINAL_SYNC_WAIT)
+    },
+    VALIDATE {
+        override val validAncestorStages = setOf(FINAL_SYNC_WAIT)
+    },
+    FINISHED {
+        override val validAncestorStages = setOf(VALIDATE)
+    },
+    ERROR {
+        override val validAncestorStages = emptySet<MigrationStage>()
+    };
 
-    FS_MIGRATION_COPY(PROVISION_MIGRATION_STACK_WAIT),
-    FS_MIGRATION_COPY_WAIT(FS_MIGRATION_COPY),
-    FS_MIGRATION_ERROR(FS_MIGRATION_COPY, FS_MIGRATION_COPY_WAIT),
+    var exception: Throwable? = null
 
-    OFFLINE_WARNING(FS_MIGRATION_COPY_WAIT),
-
-    DB_MIGRATION_EXPORT(OFFLINE_WARNING),
-    DB_MIGRATION_EXPORT_WAIT(DB_MIGRATION_EXPORT),
-    DB_MIGRATION_UPLOAD(DB_MIGRATION_EXPORT_WAIT),
-    DB_MIGRATION_UPLOAD_WAIT(DB_MIGRATION_UPLOAD),
-    DATA_MIGRATION_IMPORT(DB_MIGRATION_UPLOAD_WAIT),
-    DATA_MIGRATION_IMPORT_WAIT(DATA_MIGRATION_IMPORT),
-    FINAL_SYNC_WAIT(DATA_MIGRATION_IMPORT),
-    FINAL_SYNC_ERROR(DB_MIGRATION_EXPORT, DB_MIGRATION_EXPORT_WAIT, DB_MIGRATION_UPLOAD, DB_MIGRATION_UPLOAD_WAIT, DATA_MIGRATION_IMPORT, DATA_MIGRATION_IMPORT_WAIT, FINAL_SYNC_WAIT),
-
-    VALIDATE(FINAL_SYNC_WAIT),
-    FINISHED(VALIDATE),
-    ERROR();
-
-    private val validFromStages: List<MigrationStage>
-    var exception: Throwable?
-
-    constructor() {
-        exception = null;
-        this.validFromStages = listOf()
-    }
-
-    constructor(vararg validFromStages: MigrationStage) {
-        exception = null;
-        this.validFromStages = validFromStages.asList()
-    }
-
+    abstract val validAncestorStages: Set<MigrationStage>
 
     fun isValidTransition(to: MigrationStage): Boolean {
         when (this) {
@@ -71,18 +102,29 @@ enum class MigrationStage {
         return when (to) {
             ERROR -> true
             else -> {
-                to.validFromStages.any { it == this }
+                to.validAncestorStages.any { it == this }
             }
         }
     }
 
+
+
     fun isAfter(stage: MigrationStage): Boolean {
+        return isAfter(stage, mutableSetOf())
+    }
+
+    private fun isAfter(stage: MigrationStage, visited: MutableSet<MigrationStage>): Boolean {
         if (this == stage || this == NOT_STARTED) return false
         if (this == FINISHED || this == ERROR) return true
-        return this.validFromStages.map {
+        // If we encounter a cycle and haven't found the stage return false
+        if (visited.contains(stage)) return false
+        return this.validAncestorStages.map {
             when (it) {
                 stage -> true
-                else -> it.isAfter(stage)
+                else -> {
+                    visited.add(it)
+                    return it.isAfter(stage, visited)
+                }
             }
         }.contains(true)
     }
