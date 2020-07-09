eb01a6866376f27584999e925f7985520e12b3c6
Ben Partridge <bpartridge@atlassian.com>
2020-07-09 10:22:47 +1000

    CHET-594: [frontend] implement custom react hook for redirecting to current stage

--- frontend/src/main/dc-migration-assistant-fe/components/MigrationStageRenderer.tsx
+++ frontend/src/main/dc-migration-assistant-fe/components/MigrationStageRenderer.tsx
@@ -14,32 +14,16 @@
  * limitations under the License.
  */
 
-import React, { FunctionComponent, useState, useEffect } from 'react';
-import { Redirect } from 'react-router-dom';
+import React, { FunctionComponent } from 'react';
 import Spinner from '@atlaskit/spinner';
-import { getPathForStage } from '../utils/migration-stage-to-path';
-import { migration, MigrationStage } from '../api/migration';
+import { useCurrentStageRedirect } from '../hooks/useCurrentStageRedirect';
 
 export const MigrationStageRenderer: FunctionComponent = () => {
-    const [loading, setLoading] = useState<boolean>(true);
-    const [currentStage, setStage] = useState<MigrationStage>();
-
-    useEffect(() => {
-        setLoading(true);
-        migration
-            .getMigrationStage()
-            .then(stage => {
-                setStage(stage);
-            })
-            .catch(() => {
-                setStage(MigrationStage.ERROR);
-            })
-            .finally(() => setLoading(false));
-    }, []);
+    const loading = useCurrentStageRedirect();
 
     if (loading) {
         return <Spinner />;
     }
 
-    return <Redirect to={getPathForStage(currentStage)} />;
+    return <>I have stopped loading</>;
 };
