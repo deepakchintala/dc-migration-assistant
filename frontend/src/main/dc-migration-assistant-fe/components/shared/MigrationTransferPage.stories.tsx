e50463aff1f0afe78e107dfe058bce8ec1191e55
Ben Partridge <bpartridge@atlassian.com>
2020-07-14 16:52:32 +1000

    INNO: make migration transfer storybook cycle through process until finished

--- frontend/src/main/dc-migration-assistant-fe/components/shared/MigrationTransferPage.stories.tsx
+++ frontend/src/main/dc-migration-assistant-fe/components/shared/MigrationTransferPage.stories.tsx
@@ -1,30 +1,60 @@
 import React, { ReactNode } from 'react';
+import { MemoryRouter as Router } from 'react-router-dom';
 import { MigrationTransferPage } from './MigrationTransferPage';
 import { Progress } from './Progress';
 
 export default { title: 'Migration process' };
 
-export const notStarted = (): ReactNode => (
-    <MigrationTransferPage
-        heading="Not started migration process"
-        description="This is a migration process which has not been started yet"
-        nextText="Next"
-        nextRoute="/"
-        inProgressStages={[]}
-        startMigrationPhase={(): Promise<void> => Promise.resolve()}
-        getProgress={(): Promise<Progress[]> =>
-            Promise.resolve([
-                {
-                    phase: 'Migrating content',
-                    completeness: 0.3,
-                    elapsedTimeSeconds: 240,
-                    retryProps: {
-                        retryText: 'Retry',
-                        onRetry: (): Promise<void> => Promise.resolve(),
-                        canContinueOnFailure: false,
+let progressFetchCount = 0;
+const finishedAfterCalls = 3;
+
+export const happyPath = (): ReactNode => (
+    <Router>
+        <MigrationTransferPage
+            heading="Migrate content"
+            description="This is a migration process which has not been started yet. Once you click start, it will finish after three ticks."
+            nextText="Next step"
+            nextRoute="/"
+            inProgressStages={[]}
+            startMigrationPhase={(): Promise<void> =>
+                new Promise(resolve => setTimeout(resolve, 1000))
+            }
+            getProgress={(): Promise<Progress[]> => {
+                const retryProps = {
+                    retryText: 'Retry',
+                    onRetry: (): Promise<void> => Promise.resolve(),
+                    canContinueOnFailure: false,
+                };
+
+                if (progressFetchCount === 3) {
+                    return Promise.resolve([
+                        {
+                            phase: 'Completed content migration',
+                            completeness: 1,
+                            elapsedTimeSeconds: 18,
+                            retryProps,
+                            completeMessage: {
+                                boldPrefix: '25 of 25 files',
+                                message: 'were successfully migrated',
+                            },
+                        },
+                    ]);
+                }
+                const result = Promise.resolve([
+                    {
+                        phase: 'Migrating content',
+                        completeness: progressFetchCount / finishedAfterCalls,
+                        elapsedTimeSeconds: 6 * progressFetchCount,
+                        retryProps,
                     },
-                },
-            ])
-        }
-    />
+                ]);
+
+                if (progressFetchCount !== 3) {
+                    progressFetchCount += 1;
+                }
+
+                return result;
+            }}
+        />
+    </Router>
 );
