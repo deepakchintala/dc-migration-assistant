56cf9084de44529a202ebce3137e461e4f528b6a
Ben Partridge <bpartridge@atlassian.com>
2020-07-14 17:08:31 +1000

    INNO: add unhappy path storybook for migration transfer page

--- frontend/src/main/dc-migration-assistant-fe/components/shared/MigrationTransferPage.stories.tsx
+++ frontend/src/main/dc-migration-assistant-fe/components/shared/MigrationTransferPage.stories.tsx
@@ -2,59 +2,118 @@ import React, { ReactNode } from 'react';
 import { MemoryRouter as Router } from 'react-router-dom';
 import { MigrationTransferPage } from './MigrationTransferPage';
 import { Progress } from './Progress';
+import { MigrationStage } from '../../api/migration';
 
 export default { title: 'Migration process' };
 
-let progressFetchCount = 0;
 const finishedAfterCalls = 3;
 
-export const happyPath = (): ReactNode => (
-    <Router>
-        <MigrationTransferPage
-            heading="Migrate content"
-            description="This is a migration process which has not been started yet. Once you click start, it will finish after three ticks."
-            nextText="Next step"
-            nextRoute="/"
-            inProgressStages={[]}
-            startMigrationPhase={(): Promise<void> =>
-                new Promise(resolve => setTimeout(resolve, 1000))
-            }
-            getProgress={(): Promise<Progress[]> => {
-                const retryProps = {
-                    retryText: 'Retry',
-                    onRetry: (): Promise<void> => Promise.resolve(),
-                    canContinueOnFailure: false,
-                };
+const storybookTransferPageDefaultProps = {
+    heading: 'Migrate content',
+    nextText: 'Next step',
+    nextRoute: '/',
+    inProgressStages: new Array<MigrationStage>(),
+    startMigrationPhase: (): Promise<void> => {
+        return new Promise(resolve => setTimeout(resolve, 1000));
+    },
+};
 
-                if (progressFetchCount === 3) {
-                    return Promise.resolve([
-                        {
-                            phase: 'Completed content migration',
-                            completeness: 1,
-                            elapsedTimeSeconds: 18,
-                            retryProps,
-                            completeMessage: {
-                                boldPrefix: '25 of 25 files',
-                                message: 'were successfully migrated',
+export const happyPath = (): ReactNode => {
+    let progressFetchCount = 0;
+
+    return (
+        <Router>
+            <MigrationTransferPage
+                {...storybookTransferPageDefaultProps}
+                description="This is a migration process which has not been started yet. Once you click start, it will finish after three ticks."
+                getProgress={(): Promise<Progress[]> => {
+                    const retryProps = {
+                        retryText: 'Retry',
+                        onRetry: (): Promise<void> => Promise.resolve(),
+                        canContinueOnFailure: false,
+                    };
+
+                    if (progressFetchCount === 3) {
+                        return Promise.resolve([
+                            {
+                                phase: 'Completed content migration',
+                                completeness: 1,
+                                elapsedTimeSeconds: 18,
+                                retryProps,
+                                completeMessage: {
+                                    boldPrefix: '25 of 25 files',
+                                    message: 'were successfully migrated',
+                                },
                             },
+                        ]);
+                    }
+                    const result = Promise.resolve([
+                        {
+                            phase: 'Migrating content',
+                            completeness: progressFetchCount / finishedAfterCalls,
+                            elapsedTimeSeconds: 6 * progressFetchCount,
+                            retryProps,
                         },
                     ]);
-                }
-                const result = Promise.resolve([
-                    {
-                        phase: 'Migrating content',
-                        completeness: progressFetchCount / finishedAfterCalls,
-                        elapsedTimeSeconds: 6 * progressFetchCount,
-                        retryProps,
-                    },
-                ]);
 
-                if (progressFetchCount !== 3) {
-                    progressFetchCount += 1;
-                }
+                    if (progressFetchCount !== 3) {
+                        progressFetchCount += 1;
+                    }
 
-                return result;
-            }}
-        />
-    </Router>
-);
+                    return result;
+                }}
+            />
+        </Router>
+    );
+};
+
+export const unhappyPath = (): ReactNode => {
+    let progressFetchCount = 0;
+
+    return (
+        <Router>
+            <MigrationTransferPage
+                {...storybookTransferPageDefaultProps}
+                description="This is a migration process which has not been started yet. Once you click start, It will update twice, then fail."
+                getProgress={(): Promise<Progress[]> => {
+                    const retryProps = {
+                        retryText: 'Retry',
+                        onRetry: (): Promise<void> => {
+                            progressFetchCount = 0;
+                            return Promise.resolve();
+                        },
+                        canContinueOnFailure: false,
+                    };
+
+                    if (progressFetchCount === 3) {
+                        return Promise.resolve([
+                            {
+                                phase: 'Failed to migrate content',
+                                completeness: 0.7,
+                                elapsedTimeSeconds: 18,
+                                retryProps,
+                                errorMessage:
+                                    'Something went wrong while running your migration. Retry and see if it improves things',
+                                failed: true,
+                            },
+                        ]);
+                    }
+                    const result = Promise.resolve([
+                        {
+                            phase: 'Migrating content',
+                            completeness: progressFetchCount / finishedAfterCalls,
+                            elapsedTimeSeconds: 6 * progressFetchCount,
+                            retryProps,
+                        },
+                    ]);
+
+                    if (progressFetchCount !== 3) {
+                        progressFetchCount += 1;
+                    }
+
+                    return result;
+                }}
+            />
+        </Router>
+    );
+};
