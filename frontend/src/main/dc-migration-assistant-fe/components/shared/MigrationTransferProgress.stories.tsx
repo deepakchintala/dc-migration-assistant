53db533f2995c0478f34232e9cbd9c78c945cc37
Ben Partridge <bpartridge@atlassian.com>
2020-07-16 16:45:48 +1000

    INNO: fix prop errors with migration transfer component tests

--- frontend/src/main/dc-migration-assistant-fe/components/shared/MigrationTransferProgress.stories.tsx
+++ frontend/src/main/dc-migration-assistant-fe/components/shared/MigrationTransferProgress.stories.tsx
@@ -1,15 +1,8 @@
 import React, { ReactNode } from 'react';
 import { MigrationProgress } from './MigrationTransferProgress';
-import { RetryProperties } from './Progress';
 
 export default { title: 'Migration process progress' };
 
-const retryText = 'Retry';
-const retryProps: RetryProperties = {
-    retryText,
-    onRetry: (): Promise<void> => Promise.resolve(),
-    canContinueOnFailure: false,
-};
 const defaultCompleteMessage = {
     boldPrefix: '180GB of data',
     message: 'were restored to the target database',
@@ -21,11 +14,6 @@ export const nonDeterministicProcess = (): ReactNode => (
         progress={{
             elapsedTimeSeconds: 120,
             phase: 'Transferring your data. This may take a while...',
-            retryProps: {
-                retryText: 'Retry',
-                onRetry: (): Promise<void> => Promise.resolve(),
-                canContinueOnFailure: false,
-            },
             completeMessage: defaultCompleteMessage,
         }}
     />
@@ -37,7 +25,6 @@ export const halfwayFinishedProcess = (): ReactNode => (
         progress={{
             phase: 'Downloading data into new instance',
             completeness: 0.5,
-            retryProps,
             elapsedTimeSeconds: 360,
             completeMessage: defaultCompleteMessage,
         }}
@@ -50,7 +37,6 @@ export const failedProcess = (): ReactNode => (
         progress={{
             phase: 'Failed to install data in new instance',
             completeness: 0.7,
-            retryProps,
             elapsedTimeSeconds: 720,
             errorMessage: 'Unable to connect to target database',
             completeMessage: defaultCompleteMessage,
@@ -64,7 +50,6 @@ export const finishedProcess = (): ReactNode => (
         progress={{
             phase: 'Complete',
             completeness: 1,
-            retryProps,
             elapsedTimeSeconds: 1080,
             completeMessage: defaultCompleteMessage,
         }}
